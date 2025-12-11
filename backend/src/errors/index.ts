68bb9cccb50448fb3a996e0b8dd5010a6d80a687
Aleksandr Antoniadi <antoniadisasha@yandex.ru>
2025-12-12 02:26:08 +0500

    Исправление последних ошибок ESLint: max-classes-per-file и no-console

--- backend/src/errors/index.ts
+++ backend/src/errors/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable max-classes-per-file */
+
 // Базовый класс ошибки
 class ApiError extends Error {
   statusCode: number;
--- backend/src/middleware/requestInfo.ts
+++ backend/src/middleware/requestInfo.ts
@@ -6,11 +6,13 @@ const requestInfo = (req: Request, res: Response, next: NextFunction) => {
 
   res.locals.startTime = Date.now();
 
+  // eslint-disable-next-line no-console
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url} (ID: ${res.locals.requestId})`);
 
   const originalEnd = res.end;
   res.end = function end(chunk?: any, encoding?: any) {
     const duration = Date.now() - res.locals.startTime;
+    // eslint-disable-next-line no-console
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.url} - ${res.statusCode} (${duration}ms) (ID: ${res.locals.requestId})`);
 
     return originalEnd.call(this, chunk, encoding);
